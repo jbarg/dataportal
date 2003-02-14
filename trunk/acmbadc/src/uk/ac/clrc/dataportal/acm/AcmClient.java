879e5acc01cb53285c7b5d29190c06c61c9fce45
asm67 <asm67@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-02-14 13:23:03 +0000

    changed string to acm/services/badc

--- trunk/acmbadc/src/uk/ac/clrc/dataportal/acm/AcmClient.java
+++ trunk/acmbadc/src/uk/ac/clrc/dataportal/acm/AcmClient.java
@@ -14,7 +14,7 @@ import javax.xml.rpc.ParameterMode;
 /**
  *
  * @author  asm67
- *Class to test the functionality of ACM Web service 
+ *Class to test the functionality of ACM Web service
  *Only a test Class
  */
 public class AcmClient {
@@ -22,35 +22,37 @@ public class AcmClient {
     /** Creates a new instance of AcmClient */
     public AcmClient() {
     }
- 
+    
+  
     public static void main(String [] args) throws Exception {
+        
 
-
-       String endpoint = "http://escvig3.dl.ac.uk:" + 8080 +
-                         "/axis/services/ACM";
-
-       if (args == null || args.length != 1) {
-           System.err.println("Usage: AcmClinet <userId>");
-           return;
-       }
-       String userId = args[0];
-
-       Service  service = new Service();
-       Call     call    = (Call) service.createCall();
-
-       call.setTargetEndpointAddress( new java.net.URL(endpoint) );
-       call.setOperationName("getAccessInXMLString");
-       call.addParameter( "op1", XMLType.XSD_STRING, ParameterMode.IN );
-       // Enable this call return type if using method getAccessInXMLString
-       call.setReturnType(XMLType.XSD_STRING);
-       // Enable this call return type if using method gettAccessInW3CElement
-       //call.setReturnType( XMLType.SOAP_ELEMENT);
-       
-       // Enable print of this string if using method getAccessInXMLString
-       String ret = (String) call.invoke( new Object [] {userId});
-       System.out.println("Got result : " + ret);
-       //Enable print of this string if using method getAccessInW3CElement
-       //org.w3c.dom.Element ret = (org.w3c.dom.Element) call.invoke( new Object[] {userId});
-       //System.out.println(" Name of The Root element "+ ret.getNodeName());
-   }
+        
+        String endpoint = "http://escvig3.dl.ac.uk:" + 8080 +
+        "/acm/services/badc";
+        
+        if (args == null || args.length != 1) {
+            System.err.println("Usage: AcmClinet <userId>");
+            return;
+        }
+        String userId = args[0];
+        
+        Service  service = new Service();
+        Call     call    = (Call) service.createCall();
+        
+        call.setTargetEndpointAddress( new java.net.URL(endpoint) );
+        call.setOperationName("getAccessInXMLString");
+        call.addParameter( "op1", XMLType.XSD_STRING, ParameterMode.IN );
+        // Enable this call return type if using method getAccessInXMLString
+        call.setReturnType(XMLType.XSD_STRING);
+        // Enable this call return type if using method gettAccessInW3CElement
+        //call.setReturnType( XMLType.SOAP_ELEMENT);
+        
+        // Enable print of this string if using method getAccessInXMLString
+        String ret = (String) call.invoke( new Object [] {userId});
+        System.out.println("Got result : " + ret);
+        //Enable print of this string if using method getAccessInW3CElement
+        //org.w3c.dom.Element ret = (org.w3c.dom.Element) call.invoke( new Object[] {userId});
+        //System.out.println(" Name of The Root element "+ ret.getNodeName());
+    }
 }
