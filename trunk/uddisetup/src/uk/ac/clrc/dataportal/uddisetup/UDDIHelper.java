6d99200ff1c147d230d0c2b445c30a92e988fec8
maw24 <maw24@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-04-23 10:51:39 +0000

    Added properties file

--- trunk/uddisetup/src/uk/ac/clrc/dataportal/uddisetup/UDDIHelper.java
+++ trunk/uddisetup/src/uk/ac/clrc/dataportal/uddisetup/UDDIHelper.java
@@ -9,6 +9,7 @@ package uk.ac.clrc.dataportal.uddisetup;
 import java.util.Vector;
 import java.util.Enumeration;
 import java.util.Properties;
+import java.io.FileInputStream;
 
 import org.uddi4j.*;
 import org.uddi4j.client.*;
@@ -26,38 +27,33 @@ import org.uddi4j.transport.TransportFactory;
  *
  * @author  Mark Williams
  */
-public class UDDIHelper
-{
+public class UDDIHelper {
     
     private UDDIProxy proxy;
     private AuthToken authToken;
     private Properties tmKeyProps;
+    private Properties UDDIProps;
     
-    UDDIHelper() throws Exception
-    {
+    UDDIHelper() throws Exception {
         tmKeyProps = new Properties();
-        try
-        {
+        try {
+            UDDIProps = getUDDIProps();
             proxy = getUDDIProxy();
             authToken = getAuthToken(proxy);
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             throw e;
         }
     }
     
-    protected void finalize() throws Throwable
-    {
+    protected void finalize() throws Throwable {
         // Discard the authentication token (i.e. logout) when this object is garbage collected
-        if (authToken != null)
-        {
+        if (authToken != null) {
             proxy.discard_authToken(authToken.getAuthInfoString());
         }
     }
     
-    private static UDDIProxy getUDDIProxy()
-    {
+    private UDDIProxy getUDDIProxy() {
         // Set System properties required by UDDI4J
         System.setProperty(TransportFactory.PROPERTY_NAME, "org.uddi4j.transport.ApacheSOAPTransport"); // Use Apache SOAP Transport
         //System.setProperty("org.uddi4j.logEnabled", "true"); // Enable debugging
@@ -71,54 +67,68 @@ public class UDDIHelper
         java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         
         UDDIProxy proxy = null;
-        try
-        {
-            // URLs should really be in a property file somewhere....
+        try {
             proxy = new UDDIProxy();
-            //            proxy.setInquiryURL("http://escpc10.esc.rl.ac.uk:8080/wasp/uddi/inquiry");
-            proxy.setInquiryURL("http://escdmg.dl.ac.uk:7080/uddi/inquiry");
-            //            proxy.setPublishURL("http://escpc10.esc.rl.ac.uk:8080/wasp/uddi/publishing");
-            proxy.setPublishURL("https://escdmg.dl.ac.uk:7443/uddi/publishing");
+            
+            if (UDDIProps != null)
+            {
+                proxy.setInquiryURL(this.UDDIProps.getProperty("uddi_inquiry"));
+                proxy.setPublishURL(this.UDDIProps.getProperty("uddi_publish"));
+            }
+            else
+            {
+                // Should really throw an exception
+                System.out.println("Missing properties file!?");
+            }
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
         return proxy;
     }
     
-    private AuthToken getAuthToken(UDDIProxy proxy) throws UDDIException
-    {
+    private AuthToken getAuthToken(UDDIProxy proxy) throws UDDIException {
         AuthToken authToken = null;
-        try
-        {
-            // Username and password should also be in a property file somewhere....
-            authToken = proxy.get_authToken("dataportal","p0rt4l");
-        }
-        catch (UDDIException ue)
-        {
+        try {
+            authToken = proxy.get_authToken(UDDIProps.getProperty("uddi_user"), UDDIProps.getProperty("uddi_password"));        }
+        catch (UDDIException ue) {
             throw ue;
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
             throw new UDDIException();
         }
         return authToken;
     }
     
-    public void createTModels(Properties serviceTypes) throws UDDIException
-    {
+    private Properties getUDDIProps() throws Exception {
+        // Load UDDI Properties from file
+        Properties UDDIProps = null;
+        String propertiesFileName = "web/WEB-INF/uddi.properties";
+        
+        try {
+            System.out.println("Loading properties from " + propertiesFileName);
+            UDDIProps = new Properties();
+            FileInputStream in = new FileInputStream(propertiesFileName);
+            UDDIProps.load(in);
+            in.close();
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        
+        return UDDIProps;
+    }
+    
+    public void createTModels(Properties serviceTypes) throws UDDIException {
         // Create TModels from a properties object
-        System.out.println("Creating TModels in UDDI!");
-        try
-        {
+        System.out.println("Creating TModels in UDDI server at " + UDDIProps.getProperty("uddi_publish"));
+        try {
             // TModel Vector
             Vector tModels = new Vector();
             
             // Loop through properties files and add serviceType into the TModel Vector
-            for (Enumeration e = serviceTypes.propertyNames();e.hasMoreElements();)
-            {
+            for (Enumeration e = serviceTypes.propertyNames();e.hasMoreElements();) {
                 String serviceName = (String) e.nextElement();
                 
                 // Create a new TModel
@@ -141,25 +151,21 @@ public class UDDIHelper
             TModelDetail tmDetail = proxy.save_tModel(authToken.getAuthInfoString(),tModels);
             
             Vector tmVector = tmDetail.getTModelVector();
-            for (Enumeration e=tmVector.elements();e.hasMoreElements();)
-            {
+            for (Enumeration e=tmVector.elements();e.hasMoreElements();) {
                 TModel insertedTModel = (TModel) e.nextElement();
                 tmKeyProps.setProperty(insertedTModel.getNameString(),insertedTModel.getTModelKey());
             }
         }
-        catch (UDDIException ue)
-        {
+        catch (UDDIException ue) {
             throw ue;
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
             throw new UDDIException();
         }
     }
     
-    public Properties getTModelKeyProps()
-    {
+    public Properties getTModelKeyProps() {
         return this.tmKeyProps;
     }
     
@@ -174,22 +180,18 @@ public class UDDIHelper
         qualifier.add(new FindQualifier(FindQualifier.exactNameMatch));
         findQualifiers.setFindQualifierVector(qualifier);
         
-        try
-        {
+        try {
             TModelList tModelList = proxy.find_tModel(serviceType, null, null, findQualifiers, 1);
             Vector tModelInfoVector = tModelList.getTModelInfos().getTModelInfoVector();
-            if (!tModelInfoVector.isEmpty())
-            {
+            if (!tModelInfoVector.isEmpty()) {
                 TModelInfo tModelInfo = (TModelInfo) tModelInfoVector.elementAt(0);
                 tModelKey = tModelInfo.getTModelKey();
             }
         }
-        catch (UDDIException ue)
-        {
+        catch (UDDIException ue) {
             throw ue;
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
         return tModelKey;
