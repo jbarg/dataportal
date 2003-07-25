4606b8a0470d0fbb76ff811fe36fb02de6fe866a
nobody <nobody@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-07-25 14:17:58 +0000

    This commit was manufactured by cvs2svn to create tag 'old-version'.

---
+++ tags/old-version/xmlw-ng/src/uk/ac/cclrc/xmlwrapper/ws/selector/XmlWrapperDocSelectorService.java
@@ -0,0 +1,16 @@
+/**
+ * XmlWrapperDocSelectorService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package uk.ac.cclrc.xmlwrapper.ws.selector;
+
+public interface XmlWrapperDocSelectorService extends javax.xml.rpc.Service {
+    public java.lang.String getxmlwrapper_selectorAddress();
+
+    public uk.ac.cclrc.xmlwrapper.ws.selector.XmlWrapperDocSelector getxmlwrapper_selector() throws javax.xml.rpc.ServiceException;
+
+    public uk.ac.cclrc.xmlwrapper.ws.selector.XmlWrapperDocSelector getxmlwrapper_selector(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
