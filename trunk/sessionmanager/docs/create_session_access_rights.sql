934bcb0a44880c787a4de4f79181ba53a47768ac
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-08-22 09:48:32 +0000

    no message

--- trunk/sessionmanager/docs/create_session_access_rights.sql
+++ trunk/sessionmanager/docs/create_session_access_rights.sql
@@ -1,5 +1,5 @@
 CREATE TABLE session_access_rights (
-    sid             int,
+    sid             varchar(100),
     facility_code   varchar(10),
     metadata_access boolean,
     data_access     boolean
