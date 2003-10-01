0d898ed10ca758c08e3821bf1017d67662140d99
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-10-01 10:46:55 +0000

    no message

--- trunk/transferServlet/src/TransferDataServlet.java
+++ trunk/transferServlet/src/TransferDataServlet.java
@@ -46,63 +46,68 @@ public class TransferDataServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException{
-        ServletOutputStream out = response.getOutputStream();
-        //request users current session
-        String password = request.getParameter("passwd");
-        if(!password.equals("dpu()3^")){
-            try{
-                InetAddress host = InetAddress.getLocalHost();
-                System.out.println("wrong password");
-                out.print("Wrong password");
-                out.close();
-                return;
-                //response.sendRedirect("http://"+host.getCanonicalHostName()+":8080/dataportal/html/Login.html");
+        try{
+            ServletOutputStream out = response.getOutputStream();
+            //request users current session
+            String password = request.getParameter("passwd");
+            if(!password.equals("dpu()3^")){
+                try{
+                    InetAddress host = InetAddress.getLocalHost();
+                    System.out.println("wrong password");
+                    out.print("Wrong password");
+                    out.close();
+                    return;
+                    //response.sendRedirect("http://"+host.getCanonicalHostName()+":8080/dataportal/html/Login.html");
+                }
+                catch(Exception e){
+                    out.println(e.getMessage());
+                    out.close();
+                    System.out.println(e);
+                    return;
+                }
+                
             }
-            catch(Exception e){
-                out.println(e.getMessage());
-                out.close();
-                System.out.println(e);
-                return;
+            else{
+                
+                try {
+                    String from = request.getParameter("from");
+                    String to = request.getParameter("to");
+                    String sid = request.getParameter("sid");
+                    String cred = request.getParameter("cred");
+                    GlobusCredential globusCredential = new GlobusCredential(new FileInputStream(cred));
+                    
+                    
+                    //GlobusCredential globusCredential = new GlobusCredential(new FileInputStream("c:/v.pm"));
+                    //  GlobusCredential globusCredential = new GlobusCredential("c:/x509_gjd");
+                    
+                    //  GlobusCredential globusCredential = new GlobusCredential(System.getProperty("user.home")+"/Local Settings/Temp/x509up_u_gjd37");
+                    GSSCredential credential2 = new GlobusGSSCredentialImpl(globusCredential, GSSCredential.INITIATE_AND_ACCEPT);
+                    //System.out.println(credential2.getRemainingLifetime());
+                    
+                    UrlCopy uc = new UrlCopy();
+                    uc.setSourceUrl(new GlobusURL(from));
+                    uc.setDestinationUrl(new GlobusURL(to));
+                    uc.setUseThirdPartyCopy(true);
+                    //      uc.setDCAU(true);
+                    //      uc.setSourceAuthorization(null);
+                    //      uc.setDestinationAuthorization(null);
+                    //      uc.setAppendMode(false);
+                    uc.setCredentials(credential2);
+                    uc.copy();
+                    out.print("true");
+                    out.close();
+                }
+                catch(Exception e) {
+                    out.print(e.getMessage());
+                    out.close();
+                    System.out.println(e);
+                    return;
+                }
+                System.out.println("Transfer done");
             }
-            
         }
-        else{
-            
-            try {
-                String from = request.getParameter("from");
-                String to = request.getParameter("to");
-                String sid = request.getParameter("sid");
-                String cred = request.getParameter("cred");
-                GlobusCredential globusCredential = new GlobusCredential(new FileInputStream(cred));
-                
-                
-                //GlobusCredential globusCredential = new GlobusCredential(new FileInputStream("c:/v.pm"));
-                //  GlobusCredential globusCredential = new GlobusCredential("c:/x509_gjd");
-                
-                //  GlobusCredential globusCredential = new GlobusCredential(System.getProperty("user.home")+"/Local Settings/Temp/x509up_u_gjd37");
-                GSSCredential credential2 = new GlobusGSSCredentialImpl(globusCredential, GSSCredential.INITIATE_AND_ACCEPT);
-                //System.out.println(credential2.getRemainingLifetime());
-                
-                UrlCopy uc = new UrlCopy();
-                uc.setSourceUrl(new GlobusURL(from));
-                uc.setDestinationUrl(new GlobusURL(to));
-                uc.setUseThirdPartyCopy(true);
-                //      uc.setDCAU(true);
-                //      uc.setSourceAuthorization(null);
-                //      uc.setDestinationAuthorization(null);
-                //      uc.setAppendMode(false);
-                uc.setCredentials(credential2);
-                uc.copy();
-                out.print("true");
-                out.close();
-            }
-            catch(Exception e) {
-                out.print(e.getMessage());
-                out.close();
-                System.out.println(e);
-                return;
-            }
-            System.out.println("Transfer done");
+        catch(IOException r){
+            throw new ServletException(r.getMessage());
         }
         //System.out.println("finished");
         
