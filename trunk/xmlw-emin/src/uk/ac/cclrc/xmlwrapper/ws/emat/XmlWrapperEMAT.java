6e985bbcf2c5d133b10b75116d3869abda6f30bf
sas27 <sas27@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-10-06 09:52:46 +0000

    files had to be removed and added - weird errors about second parties !!

---
+++ trunk/xmlw-emin/src/uk/ac/cclrc/xmlwrapper/ws/emat/XmlWrapperEMAT.java
@@ -0,0 +1,12 @@
+/**
+ * XmlWrapperEMAT.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package uk.ac.cclrc.xmlwrapper.ws.emat;
+
+public interface XmlWrapperEMAT extends java.rmi.Remote {
+    public org.w3c.dom.Element getXML(java.lang.String query) throws java.rmi.RemoteException;
+}
---
+++ trunk/xmlw-emin/src/uk/ac/cclrc/xmlwrapper/ws/emat/XmlWrapperEMATService.java
@@ -0,0 +1,16 @@
+/**
+ * XmlWrapperEMATService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package uk.ac.cclrc.xmlwrapper.ws.emat;
+
+public interface XmlWrapperEMATService extends javax.xml.rpc.Service {
+    public java.lang.String getxmlwrapper_ematAddress();
+
+    public uk.ac.cclrc.xmlwrapper.ws.emat.XmlWrapperEMAT getxmlwrapper_emat() throws javax.xml.rpc.ServiceException;
+
+    public uk.ac.cclrc.xmlwrapper.ws.emat.XmlWrapperEMAT getxmlwrapper_emat(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
