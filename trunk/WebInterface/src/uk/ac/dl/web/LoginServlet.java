011e4b462db91b84b9e2e696c86fc44e6156d977
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-10-07 14:24:47 +0000

    no message

--- trunk/WebInterface/src/uk/ac/dl/web/LoginServlet.java
+++ trunk/WebInterface/src/uk/ac/dl/web/LoginServlet.java
@@ -200,7 +200,7 @@ public class LoginServlet extends HttpServlet {
                    // serializer.output(mydoc, System.out);
                   //  Saver.save(mydoc,new File("c:/mydoc.cxml"));
                   //  System.out.println("this is dir of the file "+workingDir+File.separator+"xml"+File.separator+"emin"+sessionid);
-                    Saver.save(mydoc,new File(workingDir+File.separator+"xml"+File.separator+"emat"+sessionid));
+                    Saver.save(mydoc,new File(workingDir+File.separator+"profiles"+File.separator+"emat"+sessionid));
                     //hard code list fac
                     ArrayList facs1  = new ArrayList();
                     facs1.add("EMAT");
