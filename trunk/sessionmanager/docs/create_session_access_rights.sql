be04e6b479d6f7a3953eb0bd5065d12109cf3e30
ljb53 <ljb53@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-11-20 16:43:46 +0000

    Changed table structure for new Authentication

--- trunk/sessionmanager/docs/create_session_access_rights.sql
+++ trunk/sessionmanager/docs/create_session_access_rights.sql
@@ -1,6 +1,5 @@
 CREATE TABLE session_access_rights (
     sid             varchar(100),
     facility_code   varchar(10),
-    metadata_access boolean,
-    data_access     boolean
+    permissions     varchar(1000)
 );
