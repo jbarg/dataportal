f9ee95688e11ef9cab3ff568750ad54c477c74c7
ljb53 <ljb53@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-11-20 16:32:50 +0000

    Changed permissions to 2d array of strings instead of xml

--- trunk/sessionmanager/src/Permissions.java
+++ trunk/sessionmanager/src/Permissions.java
@@ -1,158 +1,90 @@
-/*
- * Permissions.java
- *
- * Created on 08 January 2003, 17:38
- */
-
-/**
- *
- * @author  ljb53
- */
-
-/**
- * Handles XML documents to define the permissions that a user has per facility e.g
- *
- * <UserAccessPrivilege>
- *   <User>lisa_blanshard</User>
- *   <SID>155</SID>
- *   <Facility name="BADC access="t">
- *     <MetaData>t</MetaData>
- *     <Data>f</Data>
- *   </Facility>
- *   <Facility name="MPIM" access="t">
- *     <MetaData>f</MetaData>
- *     <Data>f</Data>
- *   </Facility>
- * </UserAccessPrivilege>
- */
-
 import org.apache.log4j.Logger;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.Text;
-import org.jdom.JDOMException;
-import ac.dl.xml.Converter;
-
-import java.util.List;
-import java.util.ListIterator;
-import java.util.NoSuchElementException;
+import java.sql.ResultSet;
 
 public class Permissions {
     
+    static final int MAX_FACILITIES = 20;
     static Logger logger = Logger.getLogger(Permissions.class);
-    ListIterator facList;
     
-    String facilityName;
-    void setFacilityName(String facilityName) { this.facilityName = facilityName; };
-    String getFacilityName() { return facilityName; };
-
-    String facilityAccess;
-    void setFacilityAccess(String facilityAccess) { this.facilityAccess = facilityAccess; };
-    String getFacilityAccess() { return facilityAccess; };
+    private String[][] p;
     
-    String metadata;
-    String getMetadata() { return metadata.equalsIgnoreCase("t")? "TRUE" : "FALSE"; };
-    boolean hasMetadataAccess() { return metadata.equalsIgnoreCase("t")? true : false ; };
-    void setMetadata(String metadata) { this.metadata = metadata; };
-    
-    String data;
-    String getData() { return data.equalsIgnoreCase("t")? "TRUE" : "FALSE"; };
-    boolean hasDataAccess() { return metadata.equalsIgnoreCase("t")? true : false ; };    
-    void setData(String data) { this.data = data; };
-    
-    Element root;
+    public Permissions() {
+    }
     
     /** Creates a new instance of Permissions */
-    public Permissions(org.w3c.dom.Element elem) {
-        org.w3c.dom.Document domDoc = elem.getOwnerDocument();
-        Document doc = Converter.DOMtoJDOM(domDoc);
-        root = doc.getRootElement();
+    public Permissions(String p[][]) {
+        this.p = p;
     }
     
-    // Creates a listIterator of the facilities and their permissions. Use next() to increment
-    public void buildFacilitiesList() {
-        List facilityList = root.getChildren("Facility");
-        facList = facilityList.listIterator();
-    }
-    
-    // Gets the next facility from the XML and set the facilities metadata and data permissions
-    // so they can be accessed by the calling object
-    public boolean nextFacility() {
-        Element facility;
-        try {
-            facility = (Element)facList.next();
-        } catch(NoSuchElementException e) {
-            return false;
+    public void save(String sid) throws Exception {
+        
+        // Save permissions to database
+        DBAccess db = new DBAccess();
+        db.connect();
+        
+        for (int i = 0; i < p[0].length; i++) {
+            
+            // add facility to DB if user has access
+            db.updateData("insert into session_access_rights "+
+            "(sid, facility_code, permissions) "+
+            "values ('"+sid+"', '"+p[i][1]+"', '"+p[i][0]+"')");
         }
         
-        // Sets local variables to the contents of data and metadata
-        setFacilityName(facility.getAttributeValue("name"));
-        setFacilityAccess(facility.getAttributeValue("access"));
-        setMetadata(facility.getChildText("MetaData"));
-        setData(facility.getChildText("Data"));
-        return true;
+        db.commit();
+        db.disconnect();
     }
     
-    public boolean hasAccess(String facilityName) {
-        buildFacilitiesList();
-        String t;
-        while (nextFacility()) {
-            t = getFacilityName();
-            if (t.equals(facilityName)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    public String[][] getPermissions(String sid) throws Exception {
 
-    // Reads the SID from the XML then returns it
-    public Integer getSID() {
-        Element sid = root.getChild("SID");
-        return new Integer(sid.getText());
+        // Get permissions from database
+        DBAccess db = new DBAccess();
+        db.connect();
+        ResultSet rs = db.getData("select * from session_access_rights "+
+        "where sid = '"+sid+"'");
+        
+        int i = 0;
+        this.p = new String[MAX_FACILITIES][2];
+        while (rs.next()) {
+            
+            p[i][0] = rs.getString("permissions");
+            p[i][1] = rs.getString("facility_code");
+            i++;
+        }
+        db.disconnect();
+        return p;
     }
     
-    public static org.w3c.dom.Element buildTest() throws Exception {
+    public String getPermissions(String sid, String facility) throws Exception {
         
-        /** Create test XML */
-        Document doc = new Document();
-        Element root = new Element("UserAccessPrivilege");
+        // Get permissions from database
+        DBAccess db = new DBAccess();
+        db.connect();
+        ResultSet rs = db.getData("select * from session_access_rights "+
+        "where facility_code = '"+facility+"' "+
+        "and sid = '"+sid+"'");
         
-        doc.setRootElement(root);
+        if (!rs.next()) {
+            db.disconnect();
+            throw new Exception("No rows returned for sid "+sid+" and facility "+facility);
+        }
+        db.disconnect();
+        return rs.getString("permissions");
+    }
+    
+    // Testing
+    public static String[][] buildTest() throws Exception {
+        String[][] x = {
+            {"xml string","BADC"},
+            {"xml string","MPIM"}
+        };
+        return x;
         
-        // BADC
-        Element facilityElement = new Element("Facility");
-        facilityElement.setAttribute("name","BADC");
-        
-        Element metadataElement = new Element("MetaData");
-        metadataElement.setText("t");
-        Element dataElement = new Element("Data");
-        dataElement.setText("f");
-        
-        root.addContent(facilityElement);
-        facilityElement.addContent(metadataElement);
-        facilityElement.addContent(dataElement);
-        
-        // MPIM
-        facilityElement = new Element("Facility");
-        facilityElement.setAttribute("name","MPIM");
-        
-        metadataElement = new Element("MetaData");
-        metadataElement.setText("t");
-        dataElement = new Element("Data");
-        dataElement.setText("t");
-        
-        root.addContent(facilityElement);
-        facilityElement.addContent(metadataElement);
-        facilityElement.addContent(dataElement);
-       
-        // Return w3c XML element
-        org.w3c.dom.Document domDoc;
-        domDoc = Converter.JDOMtoDOM(doc);
-        org.w3c.dom.Element finalElement = domDoc.getDocumentElement();
-        
-        logger.info("Built test xml: "+finalElement);
-        return finalElement;
-        
+    }
+    
+    public void print() throws Exception {
+        for (int i = 0; i < p[0].length; i++) {
+            System.out.println("Facility: "+p[i][1]+" permissions: "+p[i][0]);
+        }
     }
 }
 
