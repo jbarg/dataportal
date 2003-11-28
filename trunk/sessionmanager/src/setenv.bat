3c65ae9ea4e97f345d4fe3f09391365322cff6a1
ljb53 <ljb53@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-11-28 08:14:17 +0000

    no message

--- trunk/sessionmanager/src/setenv.bat
+++
@@ -1,1 +0,0 @@
-set classpath=.;d:\jdk1.4.1_01\lib\tools.jar;..\web\WEB-INF\lib\cog-jglobus.jar;..\web\WEB-INF\lib\jaxrpc.jar;..\web\WEB-INF\lib\axis.jar;..\web\WEB-INF\lib\saaj.jar;.;..\web\WEB-INF\lib\xml-dl.1.4.1.jar;.;..\web\WEB-INF\lib\jdom.jar;.;..\web\WEB-INF\lib\log4j-1.2.4.jar
