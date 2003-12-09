da6e0366178e13ed63970c3518fda8b125ca53b2
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2003-12-09 15:24:51 +0000

    no message

--- trunk/transfer/web/WEB-INF/Transfer/setEnv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set SM=http://escdmg.dl.ac.uk:8080/sessionmanager/services/SessionManager
-
-set DT=http://gjd37vig.dl.ac.uk:8080/datatransfer/services/DataTransferService
-set fromURL=http://esc.dl.ac.uk/index.html
-set toURL=gsiftp://esc.dl.ac.uk/escd1
