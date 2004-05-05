bd72b915159670957fe9ea270206d8252ffe774b
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2004-05-05 13:24:38 +0000

    no message

--- trunk/WebInterface/src/uk/ac/dl/web/SRBTransfer.java
+++ trunk/WebInterface/src/uk/ac/dl/web/SRBTransfer.java
@@ -158,8 +158,11 @@ public class SRBTransfer extends HttpServlet {
                 call.addParameter( "cert", XMLType.XSD_STRING, ParameterMode.IN );
                 call.setReturnType( XMLType.XSD_STRING );
                 InetAddress host = InetAddress.getLocalHost();
-                logger.info("http://"+host.getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/xml/"+sid+"."+format);
-                Object[] ob = new Object[]{"http://"+host.getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/xml/"+sid+"."+format,urlTo,cert};
+                //put file location for now as file instead of http
+                //logger.info("http://"+host.getHostAddress()+":8080"+request.getContextPath()+"/xml/"+sid+"."+format);
+                //Object[] ob = new Object[]{"http://"+host.getHostAddress()+":8080"+request.getContextPath()+"/xml/"+sid+"."+format,urlTo,cert};
+                logger.info("file:///"+wd+File.separator+"xml"+File.separator+sid+"."+format);
+                Object[] ob = new Object[]{"file:///"+wd+File.separator+"xml"+File.separator+sid+"."+format,urlTo,cert};
                 
                 result = (String) call.invoke(ob );
                 //save credential
