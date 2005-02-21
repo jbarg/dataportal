8d4d04b22a19aa10d14c92cc16e987ccd06e8039
rd73 <rd73@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2005-02-21 14:01:25 +0000

    Restructuring repository

--- branches/initial/ngs/ProxyManager/src/xportlets/proxymanager/Constants.java
+++
@@ -1,9 +0,0 @@
-package xportlets.proxymanager;
- 
-public final class Constants {
-    // set it to private to prevent instancelization
-    private Constants() {
-    }
-
-    public static final String PROXYSTORAGE  = "_proxy_storage";
-}
