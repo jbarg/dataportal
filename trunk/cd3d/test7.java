cffa65d701d49dca016b9d3d6368f02e263c9eec
rd73 <rd73@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2005-02-21 15:07:19 +0000

    Restructuring repository

--- trunk/cd3d/test7.java
+++
@@ -1,8 +0,0 @@
-public class test7{
-
-  public static void main( String args[] ){
-    String s = "thisis the filename.str";
-    String extension = s.substring( s.lastIndexOf( '.' )+1, s.length() );
-    System.out.println( extension );
-  }
-}
