4059c29612db1982a3453264ed6e555e4a78a7cc
rd73 <rd73@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2005-02-21 15:07:38 +0000

    Restructuring repository

--- branches/DanielHanlon/Lua/first.lua
+++
@@ -1,9 +0,0 @@
---defines a factorial function
-function factorial (n)
-	if n==0 then return 1
-	else return n*factorial(n-1) end
-end
-
-print "Enter a number:"
-a = read("*number")	--read a number
-print(factorial(a))
