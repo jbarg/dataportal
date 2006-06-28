1565b1d1175c8a9ac02b5c6ce2c50f978cf8b590
sas27 <sas27@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2006-06-28 11:07:48 +0000

    added common

---
+++ dataportal/external_modules/dpal/trunk/src/uk/ac/cclrc/dpal/beans/Common.java
@@ -0,0 +1,44 @@
+package uk.ac.cclrc.dpal.beans;
+
+public class Common
+{
+   //bean begins
+   protected String name = null ;
+   protected String id = null ;
+   protected String facility = null ;
+
+   //getters
+   public String getName() {
+      return this.name ;
+   }
+  
+   public String getId() {
+      return this.id ;
+   }
+  
+   public String getFacility() {
+      return this.facility ;
+   }
+  
+   //setters
+   public void setName(String name) {
+      this.name=name ;
+   }
+  
+   public void setId(String id) {
+      this.id=id ;
+   }
+  
+   public void setFacility(String facility) {
+      this.facility=facility ;
+   }
+ 
+   //to support printing
+   public String toString() {
+            return "[name]:"+name+
+             " [id]:"+id+
+             " [facility]:"+facility;
+   }
+
+}
+  
