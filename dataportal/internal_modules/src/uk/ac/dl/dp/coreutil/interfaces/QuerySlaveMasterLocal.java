51c3be96d9a80d72b54e9abeb020b2f803a12454
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2006-08-09 10:47:01 +0000

    initial import

---
+++ dataportal/internal_modules/src/uk/ac/dl/dp/coreutil/interfaces/QuerySlaveMasterLocal.java
@@ -0,0 +1,49 @@
+
+package uk.ac.dl.dp.coreutil.interfaces;
+
+import java.util.Collection;
+import javax.annotation.security.PermitAll;
+import javax.ejb.Local;
+import uk.ac.cclrc.dpal.beans.DataFile;
+import uk.ac.cclrc.dpal.beans.DataSet;
+import uk.ac.cclrc.dpal.beans.Investigation;
+import uk.ac.cclrc.dpal.beans.Study;
+import uk.ac.dl.dp.coreutil.clients.dto.QueryRecordDTO;
+import uk.ac.dl.dp.coreutil.exceptions.QueryException;
+import uk.ac.dl.dp.coreutil.exceptions.SessionNotFoundException;
+import uk.ac.dl.dp.coreutil.exceptions.SessionTimedOutException;
+import uk.ac.dl.dp.coreutil.exceptions.UserNotFoundException;
+
+/**
+ * This is the business interface for QuerySlaveMaster enterprise bean.
+ */
+@Local
+@PermitAll
+public interface QuerySlaveMasterLocal {
+    public String queryByKeyword(String sid, Collection<String> facilities, String[] keyword) throws QueryException,SessionNotFoundException, UserNotFoundException, SessionTimedOutException;
+    
+    public boolean isFinished();
+    
+    public void remove();
+    
+    public Collection<Study> getQueryResults();
+    
+    public Collection<String> getCompleted();
+    
+    public Collection<Investigation> getInvestigations(String sid, Collection<Study> studies) throws SessionNotFoundException, SessionTimedOutException,UserNotFoundException, QueryException;
+        
+    public Collection<DataSet> getDataSets(String sid, Collection<Investigation> investigations) throws SessionNotFoundException, SessionTimedOutException,UserNotFoundException, QueryException;
+            
+    public Collection<DataFile> getDataFiles(String sid, Collection<DataSet> datasets) throws SessionNotFoundException, SessionTimedOutException,UserNotFoundException, QueryException;
+  
+  //  public Collection<QueryRecordDTO> getAllResults(String sid) throws SessionNotFoundException, UserNotFoundException, SessionTimedOutException;
+  
+    public Collection<QueryRecordDTO> getCurrentResults(String sid);
+    
+   public Collection<Study> getPastQueryResults(String sid, String queryid);
+   
+    public Collection<Study> getPastQueryResults(String sid, QueryRecordDTO dto);
+   
+   
+      
+}
