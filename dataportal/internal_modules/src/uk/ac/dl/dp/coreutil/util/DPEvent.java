a952ca824f19a82bfc504af4ea5544e044eeaf9d
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2006-08-09 10:47:01 +0000

    initial import

---
+++ dataportal/internal_modules/src/uk/ac/dl/dp/coreutil/util/DPEvent.java
@@ -0,0 +1,33 @@
+/*
+ * Event.java
+ *
+ * Created on 22 June 2006, 13:05
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.ac.dl.dp.coreutil.util;
+
+/**
+ *
+ * @author gjd37
+ */
+public enum DPEvent {
+    
+     LOG_ON("LOG_ON"),
+    LOG_OPF("LOG_OFF"),
+    TRANSFER("TRANSFER"),
+    ADVANCED_SEARCH("ADVANCED_SEARCH"),
+    BASIC_SEARCH("BASIC_SEARCH");
+    
+    DPEvent(String value) {
+        this.value = value;
+    }
+    
+    private final String value;
+    
+    public String toString() {
+        return value;
+    }
+}
