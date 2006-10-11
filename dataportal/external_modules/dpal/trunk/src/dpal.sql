8a22cdf15f251a1fac9b8249ce65c8ec4222e287
sas27 <sas27@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2006-10-11 10:54:20 +0000

    first stab at icat3 version

--- dataportal/external_modules/dpal/trunk/src/dpal.sql
+++ dataportal/external_modules/dpal/trunk/src/dpal.sql
@@ -14,9 +14,7 @@ END;
 -- package spec:
 create or replace package dpaccess as
     function getKeywords (dn in varchar2) RETURN types.ref_cursor;
-    function getStudiesOr (keyword_array in vc_array, dn in varchar2) RETURN types.ref_cursor;
-    function getStudiesById (study_id_array in vc_array, dn in varchar2) RETURN types.ref_cursor;
-    function getInvestigations (study_id_array in num_array, dn in varchar2) RETURN types.ref_cursor;
+    function getInvestigtionsOr (keyword_array in vc_array, dn in varchar2) RETURN types.ref_cursor;
     function getInvestigationsById (inv_id_array in num_array, dn in varchar2) RETURN types.ref_cursor;
     function getDataSets (inv_id_array in num_array, dn in varchar2) RETURN types.ref_cursor;
     function getDataFiles (ds_id_array in num_array, dn in varchar2) RETURN types.ref_cursor;
@@ -29,63 +27,26 @@ create or replace package body dpaccess as
     function getKeywords (dn in varchar2)
        RETURN types.ref_cursor
     is
-       c_study types.ref_cursor;
+       c_key types.ref_cursor;
     begin
-       OPEN c_study FOR
+       OPEN c_key FOR
           select  distinct(lower(name)) as name, '-99' as  id
              from keyword order by name asc ;
-       RETURN c_study;
+       RETURN c_key;
     end;
 
-    function getStudiesOr (keyword_array in vc_array, dn in varchar2) 
-       RETURN types.ref_cursor
-    is
-       c_study types.ref_cursor;
-    begin
-       OPEN c_study FOR 
-          select * from (select  distinct(s.id) as id, s.name as name,s.start_date as start_date,s.end_date as end_date, k.name as keyword 
-             from study s, keyword_list kl, keyword k
-             where 
-                 lower(k.name) in (select * from TABLE(cast(keyword_array as VC_ARRAY)))
-               and
-                 s.id=kl.study_id
-               and
-                 kl.keyword_id=k.id  
-               and
-                 s.name is not null) 
-               where 
-                 rownum < 501 ;
-       RETURN c_study;
-    end;
-
-    function getStudiesById (study_id_array in vc_array, dn in varchar2)
-       RETURN types.ref_cursor
-    is
-       c_study types.ref_cursor;
-    begin
-       OPEN c_study FOR
-          select  s.id as id, s.name as name,s.start_date as start_date,s.end_date as end_date
-             from study s
-             where
-                 s.id in (select * from TABLE(cast(study_id_array as VC_ARRAY))) ;
-       RETURN c_study;
-    end;
-
-
-    function getInvestigations (study_id_array in num_array, dn in varchar2) 
+    function getInvestigationsOr (keyword_array in vc_array, dn in varchar2) 
        RETURN types.ref_cursor
     is
        c_inv types.ref_cursor;
     begin
-       OPEN c_inv FOR
-          select i.title as title,i.id as id,i.investigation_type as investigation_type, i.inv_abstract as inv_abstract, s.id as study_id 
-             from investigation i, investigation_list il, study s
-             where 
-                 s.id in (select * from TABLE(cast(study_id_array as NUM_ARRAY))) 
-              and
-                 i.id=il.investigation_id
-              and
-                 il.study_id=s.id ;
+       OPEN c_inv FOR 
+          select * from (select i.title as title,i.id as id,i.inv_type as investigation_type, i.inv_abstract as inv_abstract, k.name as keyword
+                            from investigation i, keyword k
+                            where
+			         lower(k.name) in (select * from TABLE(cast(keyword_array as VC_ARRAY)))
+                            and 
+                    	         i.id = k.investigation_id ) where rownum < 501 ;
        RETURN c_inv;
     end;
 
@@ -95,7 +56,7 @@ create or replace package body dpaccess as
        c_inv types.ref_cursor;
     begin
        OPEN c_inv FOR
-          select i.title as title,i.id as id,i.investigation_type as investigation_type, i.inv_abstract as inv_abstract
+          select i.title as title,i.id as id,i.inv_type as investigation_type, i.inv_abstract as inv_abstract
              from investigation i
              where
                  i.id in (select * from TABLE(cast(inv_id_array as NUM_ARRAY))) ;
@@ -108,14 +69,11 @@ create or replace package body dpaccess as
        c_ds types.ref_cursor;
     begin      
        OPEN c_ds FOR
-          select d.name as name,d.id as id,d.dataset_status as dataset_status,d.description as description,d.dataset_type as dataset_type, i.id as inv_id 
-             from dataset d, dataset_list dl, investigation i
-             where 
-                 i.id in (select * from TABLE(cast(inv_id_array as NUM_ARRAY)))
-              and
-                 d.id=dl.dataset_id
-              and
-                 dl.investigation_id=i.id ;
+             select d.name as name,d.id as id, d.dataset_status as dataset_status,d.description as description,
+                    d.dataset_type as dataset_type, d.investigation_id as inv_id
+                    from dataset d
+                    where
+                        d.investigation_id in (select * from TABLE(cast(inv_id_array as NUM_ARRAY))) ;
        RETURN c_ds;
     end;
 
@@ -125,15 +83,10 @@ create or replace package body dpaccess as
        c_df types.ref_cursor;
     begin
        OPEN c_df FOR
-          select d.name as name, d.id as id, d.uri as uri, ds.id as dataset_id
-             from datafile d, datafile_list dl, dataset ds
-             where 
-                ds.id in (select * from TABLE(cast(ds_id_array as NUM_ARRAY)))
-              and
-                d.id=dl.datafile_id 
-              and
-                dl.dataset_id=ds.id ;
-
+             select d.name as name, d.id as id, d.location as uri, d.dataset_id as dataset_id
+             from datafile d
+             where
+                d.dataset_id in (select * from TABLE(cast(ds_id_array as NUM_ARRAY))) ;
        RETURN c_df;
     end;
 
