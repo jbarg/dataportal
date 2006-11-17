7f3a8032f6cfb11d8262cf01ba2e0c3c8a12047f
sas27 <sas27@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2006-11-17 12:47:41 +0000

    added and remove various test programs

---
+++ dataportal/external_modules/dpal/branches/dpal4icat3-vanilla/src/uk/ac/cclrc/dpal/test/TestDPAccessLayerKeywordsIsis.java
@@ -0,0 +1,56 @@
+package uk.ac.cclrc.dpal.test;
+
+//for utility
+import java.io.*;
+import java.util.* ;
+
+//for catching exceptions
+import java.sql.SQLException;
+
+//for processing the beans
+import uk.ac.cclrc.dpal.beans.* ;
+ 
+public class TestDPAccessLayerKeywordsIsis
+{
+
+   public static void main(String []args)
+   {
+
+     //for results
+     ArrayList<Keyword> r_k_l = null ;
+
+     //init the dp access layer
+     String db_host = "elektra.dl.ac.uk";
+     String db_port = "1521";
+     String db_sid = "minerva2" ;
+     String db_user = "icatisis" ;
+     String db_pass = "ihave4name" ;
+     String dbConnectString = "(DESCRIPTION=(ADDRESS=(HOST="+db_host+")"+
+                             "(PROTOCOL=tcp)(PORT="+db_port+"))"+
+                             "(CONNECT_DATA=(SID="+db_sid+")))";
+
+     DPAccessLayer dpal = new DPAccessLayer("isis", dbConnectString, db_user, db_pass) ;
+
+     try
+     {
+         System.out.println("---") ;
+         System.out.println("The list of Keywords in ISIS:") ;
+         //////
+         r_k_l = dpal.getKeywords("DN") ;
+         for(Keyword k : r_k_l) {
+            System.out.println("\t"+k.toString()) ; 
+         }
+         System.out.println("---") ;
+
+         //test disconnection code
+         dpal.disconnectFromDB() ;
+      }
+      catch (SQLException sqle)
+      {
+         sqle.printStackTrace() ;
+      }
+
+      return ;
+   }
+ 
+ }
