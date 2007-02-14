4ed56f9a0acffbd497918f98f3fe4eb659512aed
ljb53 <ljb53@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2007-02-14 10:16:18 +0000

    Updated re recent data portal changes

--- dataportal/external_modules/dpal/trunk/src/emat_views.sh
+++ dataportal/external_modules/dpal/trunk/src/emat_views.sh
@@ -14,7 +14,7 @@
 
 #after the views are created dpal.sql need to be run as the metaematview user.
 
-sqlplus metaematview/viewb4rk1ng@minerva.dl.ac.uk <<EOF
+sqlplus metaematview/viewb4rk1ng <<EOF
 
 create or replace view keyword as
 select st.study_id investigation_id, t.topic name, '-99' mod_id, systimestamp mod_time
@@ -23,7 +23,7 @@ where t.leaf  = 'true'
 and t.topic_id = st.topic_id ;
 
 create or replace view investigation as
-select study_id id, name title, purpose inv_abstract,'experiment' inv_type, '-99' mod_id, row_mod_time mod_time 
+select study_id id, study_id as visit_id, name title, purpose inv_abstract,'experiment' inv_type, '-99' mod_id, row_mod_time mod_time 
 from metaematnew.study ; 
 
 create or replace view dataset as
@@ -34,6 +34,14 @@ create or replace view datafile as
 select d.data_name as name, d.data_id as id, d.dataset_id as dataset_id, d.uri as location, '-99' as mod_id, d.creation_date as mod_time
 from metaematnew.data d;
 
+create or replace view investigator as
+select study_id as investigation_id, role, person_id as facility_user_id
+from metaematnew.study_person;
+
+create or replace view facility_user as
+select dn as federal_id, forename as first_name, surname as last_name,
+person_id as facility_user_id
+from metaematnew.person;
 commit;
 exit;
 EOF
