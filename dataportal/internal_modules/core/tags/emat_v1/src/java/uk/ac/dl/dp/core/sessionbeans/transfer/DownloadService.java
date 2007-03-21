fdab62e09f0c20b4051356c849122b042071ef47
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2007-03-21 15:21:13 +0000

    version 1 for emat on adama

---
+++ dataportal/internal_modules/core/tags/emat_v1/src/java/uk/ac/dl/dp/core/sessionbeans/transfer/DownloadService.java
@@ -0,0 +1,161 @@
+/*
+ * DownloadService.java
+ *
+ * Created on 11 December 2006, 10:30
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.ac.dl.dp.core.sessionbeans.transfer;
+
+
+import java.util.Date;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import org.apache.log4j.Logger;
+import uk.ac.dl.dp.core.sessionbeans.SessionEJBObject;
+import uk.ac.dl.dp.coreutil.entity.DPConstants;
+import uk.ac.dl.dp.coreutil.entity.User;
+import uk.ac.dl.dp.coreutil.exceptions.SessionNotFoundException;
+import uk.ac.dl.dp.coreutil.exceptions.SessionTimedOutException;
+import uk.ac.dl.dp.coreutil.exceptions.UserNotFoundException;
+import uk.ac.dl.dp.coreutil.interfaces.DownloadServiceRemote;
+import uk.ac.dl.dp.coreutil.util.DataPortalConstants;
+import uk.ac.dl.dp.coreutil.util.SRBInfo;
+import uk.ac.dl.dp.coreutil.util.ServiceInfo;
+import uk.ac.dl.dp.coreutil.util.UserUtil;
+
+/**
+ *
+ * @author gjd37
+ */
+
+@Stateless(mappedName=DataPortalConstants.DOWNLOAD)
+//@WebService()
+public class DownloadService extends SessionEJBObject  implements DownloadServiceRemote {
+    
+    static final Logger log = Logger.getLogger(DownloadService.class);
+    
+    @Resource(mappedName=DataPortalConstants.CONNECTION_FACTORY)
+    private QueueConnectionFactory connectionFactory;
+    
+    @Resource(mappedName=DataPortalConstants.DOWNLOAD_MDB)
+    private Queue queue;
+    
+    public void downloadSRBFiles(String sid, SRBInfo srbInfo)  {
+        
+        //this should be done on the WSDL/Schema
+        checkArguments(srbInfo);
+        
+        QueueConnection connection = null;
+        QueueSession session = null;
+        MessageProducer messageProducer = null;
+        User user = null;
+        
+        try {
+            connection = connectionFactory.createQueueConnection();
+            session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+            messageProducer = session.createProducer(queue);
+            
+        } catch (Exception e) {
+            log.error("Unable to locate: "+DataPortalConstants.DOWNLOAD_MDB,e);
+            try {
+                //close connections
+                if(session != null) session.close();
+                if(connection != null)   connection.close();
+            } catch (JMSException ex) {}
+            return ;
+        }
+        try {
+            ObjectMessage message = session.createObjectMessage();
+            
+            user =  new UserUtil(sid,em).getUser();
+            
+            message.setObject(srbInfo);
+            messageProducer.send(message);
+            log.debug("Sending: srbDownload message for user "+user.getDn());
+            
+        } catch (JMSException ex) {
+            log.error("Unable send event: srbDownload message for user: "+user.getDn(),ex);
+        } catch (UserNotFoundException ex) {
+            log.error("No user with user sid "+sid+" in the system",ex);
+            return ;
+        } catch (SessionNotFoundException ex) {
+            log.error("No user with user sid "+sid+" in the system",ex);
+            return ;
+        }  catch (SessionTimedOutException ex) {
+            log.error("No user with user sid "+sid+" in the system",ex);
+            return ;
+        }finally{
+            try {
+                if(session != null) session.close();
+                if(connection != null)   connection.close();
+            } catch (JMSException ex) {
+                
+            }
+        }
+        
+    }
+    
+    public boolean setConstants(DPConstants constants){
+        log.debug("Setting Constants");
+        try {
+            
+            if(em.find(DPConstants.class,1L) != null){
+                log.trace("Already set");
+                return true;
+            } else {
+                log.trace("Setting ..");
+                constants.setId(1L);
+                constants.setModTime(new Date());
+                em.merge(constants);
+                log.trace("Set constants");
+                return true;
+            }
+        } catch(Exception e){
+            log.error("Unable to set constants",e);
+            return false;
+        }
+    }
+    
+    public boolean isSet(){
+        try {
+            DPConstants constants=   em.find(DPConstants.class,1L);
+            if(constants == null || constants.getDownloadLocation() == null || constants.getDownloadLocation().equals("")) return false;
+            else return true;
+        } catch(Exception e){
+            log.error("Unable to check if constants set",e);
+            return false;
+        }
+    }
+    
+    /**
+     *  This checks if the arguements are valid, should be done on the WSDL/Schema
+     */
+    private void checkArguments(SRBInfo srbInfo, ServiceInfo serviceInfo) throws IllegalArgumentException {
+        checkArguments(srbInfo);
+        if(serviceInfo.getMyProxyUsername() == null) throw new IllegalArgumentException("MyProxy username cannot be null");
+        if(serviceInfo.getMyProxyPassword() == null) throw new IllegalArgumentException("MyProxy password cannot be null");
+        if(serviceInfo.getEmail() == null) throw new IllegalArgumentException("Email address cannot be null");
+        
+    }
+    
+    /**
+     *  This checks if the arguements are valid, should be done on the WSDL/Schema
+     */
+    private void checkArguments(SRBInfo srbInfo) throws IllegalArgumentException {
+        
+        if(srbInfo.getSrbFiles() == null && srbInfo.getSrbUrls() == null) throw new IllegalArgumentException("SRBFiles and srbUrls cannot be null");
+    }
+}
+
+
---
+++ dataportal/internal_modules/core/tags/emat_v1/test/uk/ac/dl/dp5/util/SessionUtilTest.java
@@ -0,0 +1,205 @@
+/*
+ * SessionUtilTest.java
+ * JUnit based test
+ *
+ * Created on 04 August 2006, 14:01
+ */
+
+package uk.ac.dl.dp5.util;
+
+import java.util.Collection;
+import junit.framework.*;
+import java.security.cert.CertificateException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.*;
+import org.ietf.jgss.GSSCredential;
+import uk.ac.dl.dp.coreutil.clients.dto.SessionDTO;
+import uk.ac.dl.dp.coreutil.delegates.SessionDelegate;
+import uk.ac.dl.dp.coreutil.entity.Session;
+import uk.ac.dl.dp.coreutil.exceptions.SessionNotFoundException;
+import uk.ac.dl.dp.coreutil.exceptions.SessionTimedOutException;
+import uk.ac.dl.dp.coreutil.util.CachingServiceLocator;
+import uk.ac.dl.dp.coreutil.util.Certificate;
+import uk.ac.dl.dp.coreutil.util.SessionUtil;
+
+/**
+ *
+ * @author gjd37
+ */
+public class SessionUtilTest extends TestCase {
+    
+    static EntityManagerFactory emf;
+    static EntityManager em;
+    static String sid;
+    static SessionDelegate sd = SessionDelegate.getInstance();
+   
+    public SessionUtilTest(String testName) {
+        super(testName);
+    }
+    
+    protected void setUp() throws Exception {
+        if(emf == null){
+            emf = Persistence.createEntityManagerFactory("dataportal_unit_test");
+            // Create new EntityManager
+            em = emf.createEntityManager();
+            CachingServiceLocator.getInstance().put(em);
+            
+            System.out.println("logging in");
+            sid = sd.login(TestConstants.MYPROXY_USERNAME,TestConstants.MYPROXY_PASSWORD,3);
+        }
+    }
+    
+    protected void closeEnitiyManager(){
+        if(em != null){
+            // Close this EntityManager
+            System.out.println("Closing entity manager");
+            em.close();
+            System.out.println("logging out");
+            try {
+                sd.logout(sid);
+            } catch (SessionTimedOutException ex) {
+                ex.printStackTrace();
+            } catch (SessionNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    protected void tearDown() throws Exception {
+        
+        
+    }
+    
+    
+    public void testEnitityManagerConnection() throws Exception{
+        
+        
+        //If a transaction required, it is started:
+        
+        // Begin transaction
+        em.getTransaction().begin();
+        /*Collection<Session> sessions = em.createQuery("SELECT s FROM Session s").getResultList();
+         
+        for(Session ses : sessions){
+            System.out.println(ses.getUserSessionId());
+        }*/
+        
+        Session session = new SessionUtil(sid).getSession();
+        System.out.println(session.getExpireDateTime());
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+        
+        //And EntityManager should always be closed if it won't be used again:
+        
+        
+    }
+    /**
+     * Test of isValid method, of class uk.ac.dl.dp5.util.SessionUtil.
+     */
+    public void testIsValid() throws Exception {
+        System.out.println("isValid");
+        
+        // Begin transaction
+        em.getTransaction().begin();
+        
+        boolean result = new SessionUtil(sid).isValid();
+        assertEquals("Session should be vaild",true, result);
+        
+        //test bad SID only once
+        boolean result2 = false;
+        try {
+            result2 = new SessionUtil("fdffd").isValid();
+            assertEquals("Session fdffd should be invaild",false, result2);
+        } catch (SessionTimedOutException ex) {
+            ex.printStackTrace();
+        } catch (SessionNotFoundException ex) {
+            assertTrue("Session fdfdf is invalis and not found",true);
+        }
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+        
+    }
+    
+    /**
+     * Test of getCredential method, of class uk.ac.dl.dp5.util.SessionUtil.
+     */
+    public void testGetCredential() throws Exception {
+        System.out.println("getCredential");
+        
+        // Begin transaction
+        em.getTransaction().begin();
+        
+        GSSCredential result = new SessionUtil(sid).getCredential();
+        assertNotNull("GSSCredential cannot be null",result);
+        
+        assertEquals("DN should be /C=UK/O=eScience/OU=CLRC/L=DL/CN=glen drinkwater","/C=UK/O=eScience/OU=CLRC/L=DL/CN=glen drinkwater", result.getName().toString());
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+        
+    }
+    
+    /**
+     * Test of getCertificate method, of class uk.ac.dl.dp5.util.SessionUtil.
+     */
+    public void testGetCertificate() throws Exception {
+        System.out.println("getCertificate");
+        
+        // Begin transaction
+        em.getTransaction().begin();
+        
+        Certificate result = new SessionUtil(sid).getCertificate();
+        assertNotNull("Result cannot be null", result);
+        assertEquals("DN should be /C=UK/O=eScience/OU=CLRC/L=DL/CN=glen drinkwater","/C=UK/O=eScience/OU=CLRC/L=DL/CN=glen drinkwater", result.getDn());
+        assertNotSame("Lifetime must be positive", 0, result.isLifetimeLeft());
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+    }
+    
+    /**
+     * Test of getSession method, of class uk.ac.dl.dp5.util.SessionUtil.
+     */
+    public void testGetSession() throws Exception {
+        System.out.println("getSession");
+        
+        // Begin transaction
+        em.getTransaction().begin();
+        
+        Session session = new SessionUtil(sid).getSession();
+        assertNotNull("Session canot be null",session);
+        assertTrue("Expire date must be in future",session.getExpireDateTime().after(new Date()));
+        assertEquals("Sids must be equal",sid, session.getUserSessionId());
+        
+        // Commit the transaction
+        em.getTransaction().commit();        
+        
+    }
+    
+    /**
+     * Test of getSessionDTO method, of class uk.ac.dl.dp5.util.SessionUtil.
+     */
+    public void testGetSessionDTO() throws Exception{
+        System.out.println("getSessionDTO");
+        
+        // Begin transaction
+        em.getTransaction().begin();
+        
+        SessionDTO session = new SessionUtil(sid).getSessionDTO();
+        assertNotNull("SessionDTO canot be null",session);
+        assertTrue("Expire date must be in future",session.getExpireTime().after(new Date()));
+        assertEquals("Sids must be equal",sid, session.getUserSessionId());
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+        
+        closeEnitiyManager();
+    }
+    
+}
