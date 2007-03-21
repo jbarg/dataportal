8b0da9e832b7efcbee46b1b6d94cbcc7782bdef5
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2007-03-21 15:10:13 +0000

    A    https://esc-cvs.dl.ac.uk/svn/dl/dataportal/internal_modules/coreutil/tags/emat_v1

---
+++ dataportal/internal_modules/coreutil/tags/emat_v1/src/uk/ac/dl/dp/coreutil/delegates/DownloadDelegate.java
@@ -0,0 +1,54 @@
+package uk.ac.dl.dp.coreutil.delegates;
+
+import java.net.MalformedURLException;
+import javax.naming.NamingException;
+import uk.ac.dl.dp.coreutil.entity.DPConstants;
+import uk.ac.dl.dp.coreutil.exceptions.LoginMyProxyException;
+import uk.ac.dl.dp.coreutil.interfaces.DownloadServiceRemote;
+import uk.ac.dl.dp.coreutil.util.CachingServiceLocator;
+import uk.ac.dl.dp.coreutil.util.DataPortalConstants;
+import uk.ac.dl.dp.coreutil.util.SRBInfo;
+
+/**
+ *
+ * @author gjd37
+ */
+public class DownloadDelegate {
+    
+    private static DownloadDelegate dd;
+    private static DownloadServiceRemote ds ;
+    
+    public static DownloadDelegate getInstance(){
+        synchronized(DownloadDelegate.class){
+            if(dd == null){
+                try {
+                    dd = new DownloadDelegate();
+                } catch(Exception se) {
+                    throw new RuntimeException(se);
+                }
+            }
+            return dd;
+        }
+    }
+    
+    /** Creates a new instance of SessionDelegate */
+    private  DownloadDelegate() throws NamingException {
+        CachingServiceLocator csl =  CachingServiceLocator.getInstance();
+        ds  = (DownloadServiceRemote)csl.lookup(DataPortalConstants.DOWNLOAD);
+    }
+    
+     public void downloadSRBFiles(String sid, SRBInfo srbInfo){
+         ds.downloadSRBFiles(sid,srbInfo);
+     }
+  
+     public boolean setConstants(DPConstants constants){
+         return ds.setConstants(constants);
+     }
+     
+     public boolean isSet(){
+         return ds.isSet();
+         
+     }
+    
+    
+}
