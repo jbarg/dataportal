ea5fa814ae0617711f0391346604f5e88e4194f9
sas27 <sas27@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2007-04-18 08:28:24 +0000

    deployed on kisumu.esc.rl.ac.uk on April 18th 2007 - part of DLS phase 1

---
+++ dataportal/external_modules/dpal/tags/diamond-v1/src/emat_views.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#sas27
+#to allow emat/emin schema to use dpal these views are applied
+#not an external schema which can view the emat/emin schema is used
+#to create the views as emat/emin.dataset and icat.dataset names clash otherwise
+
+#priveledges needed by the view schema:
+#GRANT CONNECT TO metaematview;
+#GRANT CREATE TABLE TO metaematview;
+#GRANT CREATE VIEW TO metaematview;
+#GRANT CREATE PROCEDURE TO metaematview;
+#GRANT CREATE TYPE TO metaematview;
+
+#after the views are created dpal.sql need to be run as the metaematview user.
+
+sqlplus metaematview/viewb4rk1ng <<EOF
+
+create or replace view keyword as
+select st.study_id investigation_id, t.topic name, '-99' mod_id, systimestamp mod_time
+from metaematnew.study_topic st, metaematnew.topic t  
+where t.leaf  = 'true' 
+and t.topic_id = st.topic_id ;
+
+create or replace view investigation as
+select study_id id, study_id as visit_id, name title, purpose inv_abstract,'experiment' inv_type, '-99' mod_id, row_mod_time mod_time 
+from metaematnew.study ; 
+
+create or replace view dataset as
+select d.study_id investigation_id, d.dataset_name name, d.dataset_id id, 'complete' dataset_status, d.description description, 'data' dataset_type, '-99' mod_id, d.creation_date mod_time
+from metaematnew.dataset d ;
+
+create or replace view datafile as 
+select d.data_name as name, d.data_id as id, d.dataset_id as dataset_id, d.uri as location, '-99' as mod_id, d.creation_date as mod_time
+from metaematnew.data d;
+
+create or replace view investigator as
+select study_id as investigation_id, role, person_id as facility_user_id
+from metaematnew.study_person;
+
+create or replace view facility_user as
+select dn as federal_id, forename as first_name, surname as last_name,
+person_id as facility_user_id
+from metaematnew.person;
+commit;
+exit;
+EOF
---
+++ dataportal/external_modules/dpal/tags/diamond-v1/src/uk/ac/cclrc/dpal/beans/DataSet.java
@@ -0,0 +1,62 @@
+package uk.ac.cclrc.dpal.beans;
+
+import java.io.Serializable;
+
+public class DataSet extends Common implements Serializable
+{
+   //bean begins
+   private String dataSetStatus = null ;
+   private String dataSetType = null ;
+   private String description = null ;
+   private String investigationId = null ;
+
+   //getters
+   public String getDataSetStatus() {
+      return this.dataSetStatus ;
+   }
+  
+   public String getDataSetType() {
+      return this.dataSetType ;
+   }
+  
+   public String getDescription() {
+      return this.description ;
+   }
+ 
+   public String getInvestigationId() {
+      return this.investigationId ;
+   }
+ 
+   //setters
+   public void setDataSetStatus(String dataSetStatus) {
+      this.dataSetStatus=dataSetStatus ;
+   }
+  
+   public void setDataSetType(String dataSetType) {
+      this.dataSetType=dataSetType ;
+   }
+  
+   public void setDescription(String description) {
+      this.description=description ;
+   }
+
+   public void setInvestigationId(String investigationId) {
+      this.investigationId=investigationId ;
+   }
+
+   //support for printing
+   public String toString() {
+            return "[name]:"+getName()+
+             " [id]:"+getId()+
+             " [status]:"+dataSetStatus+
+             " [type]:"+dataSetType+
+             " [description]:"+description+
+             " [facility]:"+getFacility()+
+             " [investigation id]:"+investigationId ;
+   }
+
+   public String getDpId(){
+       return getFacility()+"-"+getId();
+   }
+}
+  
