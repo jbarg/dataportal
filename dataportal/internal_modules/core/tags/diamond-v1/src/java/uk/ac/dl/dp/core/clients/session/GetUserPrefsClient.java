be7952879e9ea8847544bcc6b8e35f1564bd30ec
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2007-04-18 08:23:21 +0000

    first release to diamond on kisumu on the 23rd April

---
+++ dataportal/internal_modules/core/tags/diamond-v1/src/java/uk/ac/dl/dp/core/clients/session/GetUserPrefsClient.java
@@ -0,0 +1,62 @@
+/*
+ * NewMain.java
+ *
+ * Created on 31 March 2006, 15:45
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.ac.dl.dp.core.clients.session;
+
+import javax.naming.*;
+import java.util.*;
+import uk.ac.dl.dp.coreutil.clients.dto.UserPreferencesDTO;
+import uk.ac.dl.dp.coreutil.interfaces.SessionRemote;
+import uk.ac.dl.dp.coreutil.util.DPResolution;
+import uk.ac.dl.dp.coreutil.util.DataPortalConstants;
+
+/**
+ *
+ * @author gjd37
+ */
+public class GetUserPrefsClient {
+    
+    /** Creates a new instance of NewMain */
+    public GetUserPrefsClient() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try{
+            
+            InitialContext ic = new InitialContext();
+            
+            SessionRemote sless = (SessionRemote) ic.lookup(DataPortalConstants.SESSION);
+            
+            
+                    
+           UserPreferencesDTO dto =  sless.getUserPrefs("565a63dd-5715-4ef2-aac3-c69213082d36");
+           
+            System.out.println("Got session : "+dto.getResolution());
+            //
+        }catch(Exception e){
+            
+            e.printStackTrace();
+            
+        }
+    }
+    
+    public static Context getContext() throws Exception{
+        Hashtable env = new Hashtable();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+        env.put(Context.PROVIDER_URL, "localhost");
+        env.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces" );
+        Context ctx = new InitialContext(env);
+        return ctx;
+    }
+    
+}
