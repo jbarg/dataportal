5513275572871bc5b9c5da680185aef9107c49da
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2007-06-22 11:53:02 +0000

    first ws

---
+++ dataportal/internal_modules/coreutil/tags/ws-v1/src/uk/ac/dl/dp/coreutil/interfaces/QueryRemote.java
@@ -0,0 +1,60 @@
+
+package uk.ac.dl.dp.coreutil.interfaces;
+
+import java.util.Collection;
+import javax.ejb.Remote;
+import uk.ac.cclrc.dpal.beans.DataFile;
+import uk.ac.cclrc.dpal.beans.DataSet;
+import uk.ac.cclrc.dpal.beans.Investigation;
+import uk.ac.cclrc.dpal.beans.Keyword;
+
+import uk.ac.dl.dp.coreutil.clients.dto.QueryRecordDTO;
+import uk.ac.dl.dp.coreutil.exceptions.QueryException;
+import uk.ac.dl.dp.coreutil.util.DPQueryType;
+import uk.ac.dl.dp.coreutil.util.QueryRequest;
+import uk.ac.cclrc.dpal.enums.LogicalOperator;
+import uk.ac.dl.dp.coreutil.exceptions.SessionException;
+
+/**
+ * This is the business interface for QuerySlaveMaster enterprise bean.
+ */
+@Remote
+public interface QueryRemote {
+    public QueryRequest query(String sid, Collection<String> facilities, String[] keyword, LogicalOperator logicalOperator, boolean fuzzy, DPQueryType queryType) throws QueryException,SessionException;
+    
+    public boolean isFinished(QueryRequest request);
+    
+    public Collection<Investigation> getQueryResults(String queryId);
+    
+    public Collection<Investigation> getQueryResults(QueryRequest request);
+    
+    public Collection<String> getCompleted(QueryRequest request);
+    
+    //   public Collection<Investigation> getInvestigationByStudyId(String sid, String fac, String studyId) throws SessionNotFoundException, UserNotFoundException, SessionTimedOutException, QueryException;
+    
+    public Collection<Investigation> getInvestigationById(String sid, String fac, String investigationId) throws SessionException,QueryException;
+    
+    public Collection<Keyword> getKeywordsByInvestigationId(String sid,  Collection<Investigation> investigastionId) throws SessionException, QueryException;
+    
+    
+    //  public Collection<Investigation> getInvestigations(String sid, Collection<Study> studies) throws SessionNotFoundException, SessionTimedOutException,UserNotFoundException, QueryException;
+    
+    // public Collection<DataSet> getDataSets(String sid, String[][] investigations) throws SessionNotFoundException, SessionTimedOutException,UserNotFoundException, QueryException;
+    
+    public Collection<DataSet> getDataSets(String sid, Collection<Investigation> investigations) throws SessionException, QueryException;
+    
+    public Collection<DataFile> getDataFiles(String sid, Collection<DataSet> datasets) throws SessionException, QueryException;
+    
+    //  public Collection<QueryRecordDTO> getAllResults(String sid) throws SessionNotFoundException, UserNotFoundException, SessionTimedOutException;
+    
+    public Collection<QueryRecordDTO> getCurrentResults(String sid);
+    
+    public Collection<Investigation> getPastQueryResults(String sid, String queryid);
+    
+    public Collection<Investigation> getPastQueryResults(String sid, QueryRecordDTO dto);
+    
+    public String[] getKeywords(String facility) throws QueryException;
+    
+    public String[] getKeywords(String facility, boolean redownload) throws QueryException;
+    
+}
---
+++ dataportal/internal_modules/coreutil/tags/ws-v1/src/uk/ac/dl/dp/coreutil/util/QueryRecord.java
@@ -0,0 +1,150 @@
+/*
+ * QueryRecord.java
+ *
+ * Created on 30 June 2006, 10:00
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.ac.dl.dp.coreutil.util;
+
+
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.Collection;
+import uk.ac.cclrc.dpal.beans.Investigation;
+
+
+
+
+/**
+ *
+ * @author gjd37
+ */
+public class QueryRecord implements Serializable {
+    private Timestamp sent;
+    private Timestamp processed;
+    private Collection<Investigation> result;
+    private String id;
+    private String queryid;
+    private String DN;
+    private String sid;
+    private Exception exception;
+    private String fac;
+    private Collection<String> facilities;
+    
+    /*public QueryRecord(String id, String queryid,String fac,String DN, String sid, Timestamp sent, Timestamp processed, Collection<Study> result,Exception ex) {
+        this.setSent(sent);
+        this.setProcessed(processed);
+        this.setResult(result);
+        this.setId(id);
+        this.setQueryid(queryid);
+        this.setDN(DN);
+        this.setSid(sid);
+        this.setException(ex);
+        this.setFac(fac);
+    }*/
+    
+    public QueryRecord(QueryRequest qr, Timestamp processed, Collection<Investigation> result, Exception ex) {
+        this.setSent(new Timestamp(qr.getSent().getTime()));
+        this.setProcessed(processed);
+        this.setResult(result);
+        this.setId(qr.getId());
+        this.setQueryid(qr.getQueryid());
+        this.setDN(qr.getDN());
+        this.setSid(qr.getSid());
+        this.setException(ex);
+        this.setFac(qr.getFacility());
+        this.setFacilities(qr.getFacilities());
+    }
+    
+    public Timestamp getSent() {
+        return sent;
+    }
+    
+    public void setSent(Timestamp sent) {
+        this.sent = sent;
+    }
+    
+    public Timestamp getProcessed() {
+        return processed;
+    }
+    
+    public void setProcessed(Timestamp processed) {
+        this.processed = processed;
+    }
+    
+    public Collection<Investigation> getResult() {
+        return result;
+    }
+    
+    public void setResult(Collection<Investigation> result) {
+        this.result = result;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getSid() {
+        return sid;
+    }
+    
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+    
+    public String toString(){
+        StringBuilder b = new StringBuilder();
+        for(Investigation study : result){
+            b.append(study.getName()+", ");
+        }
+        
+        return "QueryRecord[id: "+getId()+"] [sid: "+getSid()+"] [result: "+getResult().size()+"] [Studies: "+b+"]";
+    }
+    
+    public Exception getException() {
+        return exception;
+    }
+    
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+    
+    public String getQueryid() {
+        return queryid;
+    }
+    
+    public void setQueryid(String queryid) {
+        this.queryid = queryid;
+    }
+    
+    public String getDN() {
+        return DN;
+    }
+    
+    public void setDN(String DN) {
+        this.DN = DN;
+    }
+    
+    public String getFac() {
+        return fac;
+    }
+    
+    public void setFac(String fac) {
+        this.fac = fac;
+    }
+
+    public Collection<String> getFacilities() {
+        return facilities;
+    }
+
+    public void setFacilities(Collection<String> facilities) {
+        this.facilities = facilities;
+    }
+}
