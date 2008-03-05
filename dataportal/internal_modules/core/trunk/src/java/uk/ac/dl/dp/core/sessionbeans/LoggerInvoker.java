8a68de2e32d1a13771913a7f0ed510afbe7fbef2
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2008-03-05 16:14:44 +0000

    tidy up

--- dataportal/internal_modules/core/trunk/src/java/uk/ac/dl/dp/core/sessionbeans/LoggerInvoker.java
+++ dataportal/internal_modules/core/trunk/src/java/uk/ac/dl/dp/core/sessionbeans/LoggerInvoker.java
@@ -6,22 +6,15 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package uk.ac.dl.dp.core.sessionbeans;
 
-import javax.interceptor.AroundInvoke;
-import javax.interceptor.InvocationContext;
 import org.apache.log4j.Logger;
 
 /**
  *
  * @author gjd37
  */
-public class LoggerInvoker  {
-    
+public class LoggerInvoker {
+
     static Logger log = Logger.getLogger(LoggerInvoker.class);
-   
-    
-   
-    
 }
