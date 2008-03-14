912db590381dfe4703c8202367f00a0690317c5f
gjd37 <gjd37@43528596-e3b3-11dd-a8eb-ddf07f3c1838>
2008-03-14 13:54:34 +0000

    web service wrapper for DP and ICAT API

---
+++ dataportal/internal_modules/icat-ws/src/uk/ac/dp/icatws/ICATSingleton.java
@@ -0,0 +1,51 @@
+/*
+ * ICATSingleton.java
+ *
+ * Created on 25 June 2007, 10:33
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.ac.dp.icatws;
+
+import java.util.HashMap;
+import javax.xml.ws.BindingProvider;
+
+/**
+ *
+ * @author gjd37
+ */
+public class ICATSingleton {
+    
+    private static ICATService icatService = new ICATService();
+    private static HashMap<String, ICAT> webServicePorts = new HashMap<String, ICAT>();
+    
+    /** Creates a new instance of ICATSingleton */
+    private ICATSingleton() {
+    }
+    
+    public static synchronized ICAT getInstance(String wsdlLocation){
+        
+        if(wsdlLocation == null) throw new IllegalArgumentException("wsdlLocation cannot be null");
+        ICAT port = webServicePorts.get(wsdlLocation);
+        if(port == null){
+            System.out.println("Port is null, getting port for "+wsdlLocation);
+            port = getPort(wsdlLocation);
+        } else{
+            //set the wsdl location
+            //  ((BindingProvider)port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, wsdlLocation);
+        }
+        return port;
+        
+    }
+    
+    private static ICAT getPort(String wsdlLocation){
+        ICAT port = icatService.getICATPort();
+        //set the wsdl location
+        ((BindingProvider)port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, wsdlLocation);
+        //add to hashmap
+        webServicePorts.put(wsdlLocation, port);
+        return port;
+    }
+}
